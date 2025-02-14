3d71427fe809d3e18576ffc60be39868a6a262ae
Imlapta <113951333+Imlapta@users.noreply.github.com>
2025-02-14 17:20:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ from tglogging import TelegramLogHandler
 
 # Config 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7053921138:AAGoQZh2x00uTuufuzqEqDYO7YlWQa7KmF8")
-    API_ID = int(os.environ.get("API_ID",  "27498866"))
-    API_HASH = os.environ.get("API_HASH", "96fbb6ad2e11ab04e83ca09ef3f42455")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7902778109:AAG6uGwh-C6_STHbjsnXFKgZaJsiYHgtMEQ")
+    API_ID = int(os.environ.get("API_ID",  "3737117"))
+    API_HASH = os.environ.get("API_HASH", "f466ca6ff400954d192ce0992ddf8b5d")
     DOWNLOAD_LOCATION = "./DOWNLOADS"
     SESSIONS = "./SESSIONS"
 
-    AUTH_USERS = os.environ.get('AUTH_USERS', '6488911325').split(',')
+    AUTH_USERS = os.environ.get('AUTH_USERS', '1110590703').split(',')
     for i in range(len(AUTH_USERS)):
         AUTH_USERS[i] = int(AUTH_USERS[i])
 
